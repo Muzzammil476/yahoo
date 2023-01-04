f3c70bb1cd57559b481c287b909e96c20b98682b
ahsaan ali <ahsaanali476@gmail.com>
2023-01-04 16:42:46 +0530

    firstcommit

---
+++ Deployment.sh
@@ -0,0 +1,15 @@
+apiVersion: v1
+kind: Deployment
+metadata: 
+  - name: apps
+    namespace: test-ns
+spec:
+  selector:
+   matchLabels:
+     apps: file
+  container:
+   - name: containerd
+     image: dockerhandson
+     port:
+     - port: 8080
+       containerPort: 8080
